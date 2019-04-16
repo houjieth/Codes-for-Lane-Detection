07a86883cdc2f82cbb4dd9ef03f08b2083919946
Jie Hou <jie@pony.ai>
2019-04-15 20:37:57 -0700

    Overlay display lane

--- SCNN-Tensorflow/lane-detection-model/tools/test_lanenet.py
+++ SCNN-Tensorflow/lane-detection-model/tools/test_lanenet.py
@@ -25,6 +25,8 @@ from lanenet_model import lanenet_merge_model
 from config import global_config
 from data_provider import lanenet_data_processor_test
 
+import numpy as np
+
 
 CFG = global_config.cfg
 # VGG_MEAN = [103.939, 116.779, 123.68]  # RGB?
@@ -81,26 +83,48 @@ def test_lanenet(dataset_dir, image_path, weights_path, use_gpu, image_list, bat
         sess.run(tf.global_variables_initializer())
         saver.restore(sess=sess, save_path=weights_path)
         for i in range(math.ceil(len(image_list) / batch_size)):
-            print(i)
+            print("Inferring batch {} with batch size {}".format(i, batch_size))
             paths = test_dataset.next_batch()
             instance_seg_image, existence_output = sess.run([binary_seg_ret, instance_seg_ret],
                                                             feed_dict={input_tensor: paths})
-            for cnt, image_name in enumerate(paths):
-                print(image_name)
-                parent_path = os.path.dirname(image_name)
-                directory = os.path.join(save_dir, 'vgg_SCNN_DULR_w9', parent_path)
-                if not os.path.exists(directory):
-                    os.makedirs(directory)
-                file_exist = open(os.path.join(directory, os.path.basename(image_name)[:-3] + 'exist.txt'), 'w')
+            # pudb.set_trace()
+            for cnt, input_image_path in enumerate(paths):
+                print("Generating output for input image {}".format(input_image_path))
+                input_image = cv2.imread(input_image_path)
+                input_image = cv2.resize(input_image, (CFG.TRAIN.IMG_WIDTH, CFG.TRAIN.IMG_HEIGHT))
+
+                all_lanes_image = np.zeros((CFG.TRAIN.IMG_HEIGHT, CFG.TRAIN.IMG_WIDTH))
+
+                parent_path = os.path.dirname(input_image_path)
+                output_dir = os.path.join(parent_path, save_dir)
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
+
+                file_exist = open(os.path.join(output_dir, os.path.basename(input_image_path)[:-3] + 'exist.txt'), 'w')
                 for cnt_img in range(4):
-                    filename = os.path.join(directory, os.path.basename(image_name)[:-4] + '_' + str(cnt_img + 1) + '_avg.png')
-                    print("writing file to {}".format(filename))
-                    cv2.imwrite(filename, (instance_seg_image[cnt, :, :, cnt_img + 1] * 255).astype(int))
+                    filename = os.path.join(output_dir, os.path.basename(input_image_path)[:-4] + '_' + str(cnt_img + 1) + '_avg.png')
+                    lane_image = (instance_seg_image[cnt, :, :, cnt_img + 1] * 255).astype(int)
+                    cv2.imwrite(filename, lane_image)
+                    all_lanes_image = all_lanes_image + lane_image
                     if existence_output[cnt, cnt_img] > 0.5:
                         file_exist.write('1 ')
                     else:
                         file_exist.write('0 ')
                 file_exist.close()
+
+                # Make all_lanes_image into color image (3 channels)
+                all_lanes_color_image = np.zeros((CFG.TRAIN.IMG_HEIGHT, CFG.TRAIN.IMG_WIDTH, 3))
+                all_lanes_color_image[:, :, 2] = all_lanes_image[:, :]
+
+                all_lanes_color_image_path = os.path.join(output_dir, os.path.basename(input_image_path)[:-3] + 'lanes.jpg')
+                resized_input_image_path = os.path.join(output_dir, os.path.basename(input_image_path)[:-3] + 'resized_input.jpg')
+                overlay_image_path = os.path.join(output_dir, os.path.basename(input_image_path)[:-3] + 'overlay.jpg')
+
+                cv2.imwrite(resized_input_image_path, input_image)
+                cv2.imwrite(all_lanes_color_image_path, all_lanes_color_image)
+                cv2.imwrite(overlay_image_path, all_lanes_color_image + input_image)
+
+
     sess.close()
     return
 
